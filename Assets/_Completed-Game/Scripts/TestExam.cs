40299bf1ef5c33a64b3d8f5bd455e699cd5833f9
guanrui2 <45959141+guanrui2@users.noreply.github.com>
2018-12-26 22:13:07 +0800

    Add files via upload

---
+++ Assets/_Completed-Game/Scripts/TestExam.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestExam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.LogError ("11111111");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
