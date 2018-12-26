6971f7b1ca35a6ebc804fb2f7e3ad90787418c93
Rui Guan <guanrui@linqsoft.com>
2018-12-27 00:12:29 +0800

    Revert "updata"
    This reverts commit 7bb99c3eeb657105ddc7f4879136d5b63a0717f2.

--- Assets/_Completed-Game/Scripts/TestExam.cs
+++ Assets/_Completed-Game/Scripts/TestExam.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +7,6 @@ public class TestExam : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Debug.LogError ("11111111");
-Debug.LogError ("guanrui_work");
 	}
 	
 	// Update is called once per frame
